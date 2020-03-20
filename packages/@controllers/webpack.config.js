54779a80dc38288cf4bacb6d3d7008323702e6f9
Hopsoft <natehop@gmail.com>
2020-03-20 17:48:21 -0600

    Experiments with building all

--- packages/@controllers/webpack.config.js
+++ packages/@controllers/webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-class-properties'
+            ]
+          }
+        }
+      }
+    ]
   }
 }
